953ff470ae1f39db7b539d00b66a0bb5c939d91c
roshanyohannan <roshan.yohannan12345@gmail.com>
2022-11-21 17:52:06 -0800

    Change tabs to spaces

--- include/Core/Expression.h
+++ include/Core/Expression.h
@@ -7,34 +7,34 @@ namespace Speedy {
 
 struct Dims
 {
-	Dims(int r, int c) : rows(r), cols(c)
-	{ }
+    Dims(int r, int c) : rows(r), cols(c)
+    { }
 
-	Dims()
-	{ }
+    Dims()
+    { }
 
-	int rows = 0;
-	int cols = 0;
+    int rows = 0;
+    int cols = 0;
 };
 
 
 class Expression
 {
 public:
-	virtual int 	operator()	(int row, int col)	const = 0;
-	virtual Dims 	size		()					const = 0;
+    virtual int     operator()  (int row, int col)  const = 0;
+    virtual Dims    size        ()                  const = 0;
 };
 
 
 
 bool operator== (const Dims& left, const Dims& right)
 {
-	return (left.rows == right.rows) && (left.cols == right.cols);
+    return (left.rows == right.rows) && (left.cols == right.cols);
 }
 
 bool operator!= (const Dims& left, const Dims& right)
 {
-	return !(left == right);
+    return !(left == right);
 }
 
 
